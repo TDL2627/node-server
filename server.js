4ec2f3c12e35274536e4b1d2c7f3c60509634223
TDL2627 <the.dark.lord.2627@gmail.com>
2022-02-08 08:39:30 +0200

    upload

--- server.js
+++ server.js
@@ -1,33 +1,11 @@
 const http = require('http');
 
 const server = http.createServer(function(req, res){
-    if (req.url == '/') { //check the URL of the current request
-        
-        // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        
-        // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
-    
-    }
-    else if (req.url == "/student") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
-        res.end();
-    
-    }
-    else if (req.url == "/admin") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
-    
-    }
-    else
-        res.end('Invalid Request!');
-
+    if (req.url == '/data') { //check the URL of the current request
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: "Hello World"}));  
+        res.end();  
+}
 
 });
 server.listen(2627, function(){
